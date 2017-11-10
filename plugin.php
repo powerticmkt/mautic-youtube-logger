0d82b50d6f03847a73e4df52f9c8b56f55215a43
Luiz Eduardo Oliveira Fonseca <luizeof@gmail.com>
2017-11-10 14:52:13 -0200

    Beta

--- plugin.php
+++ plugin.php
@@ -20,7 +20,39 @@ function mautic_youtube( $atts ) {
 		'mautic_youtube'
 	);
 
-	$res = include 'iframe.php';
+	$res = <<<EOT
+	<iframe id="player" height="{$atts['height']}" src="https://www.youtube.com/embed/{$atts['videoId']}?enablejsapi=1"></iframe>
+	<script>
+	  var player;
+	  var mauticUrl = {$atts['mauticUrl']};
+	  function onYouTubeIframeAPIReady() {
+	    player = new YT.Player('player', {
+	      events: {
+	        'onStateChange': onPlayerStateChange
+	      }
+	    });
+	  }
+	  function onPlayerStateChange(event) {
+	    var videoUrl = event.target.getVideoUrl();
+	    var videoTitle = player.getVideoData()['title'];
+	    var videoid = player.getVideoData()['video_id'];
+	    var video_tag = 'youtube_' + videoid;
+	    var video_end_tag = video_tag + "_end";
+	    switch (event.data) {
+	      case 0:
+	        tracking = new Image();
+	        tracking.src = mauticUrl + '/mtracking.gif?tags=' + video_tag + ',' + video_end_tag + '&page_url=' + videoUrl + '&page_title=' + videoTitle;
+	        document.body.appendChild(tracking);
+	        break;
+	      case 1:
+	        tracking = new Image();
+	        tracking.src = mauticUrl + '/mtracking.gif?tags=' + video_tag + '&page_url=' + videoUrl + '&page_title=' + videoTitle;;
+	        document.body.appendChild(tracking);
+	    }
+	  }
+	</script>
+	<script src="https://www.youtube.com/iframe_api"></script>
+EOT;
 
 	return $res;
 
