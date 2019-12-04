89dee3cf74997f0e86d67c5e66651a67c62a01dc
Luiz Eduardo <luizeof@gmail.com>
2019-12-04 09:27:53 -0300

    updates

--- plugin.php
+++ plugin.php
@@ -36,17 +36,13 @@ $res = <<<EOT
 	    var videoid = player.getVideoData()['video_id'];
 	    var video_tag = 'youtube_' + videoid;
 	    var video_end_tag = video_tag + "_end";
-	    switch (event.data) {
-	      case 0:
-	        tracking = new Image();
-	        tracking.src = mauticUrl + '/mtracking.gif?tags=' + video_tag + ',' + video_end_tag + '&page_url=' + videoUrl + '&page_title=' + videoTitle;
-	        document.body.appendChild(tracking);
-	        break;
-	      case 1:
-	        tracking = new Image();
-	        tracking.src = mauticUrl + '/mtracking.gif?tags=' + video_tag + '&page_url=' + videoUrl + '&page_title=' + videoTitle;;
-	        document.body.appendChild(tracking);
-	    }
+			switch (event.data) {
+				case 1:
+					tracking = new Image();
+					tracking.src = mauticUrl + '/mtracking.gif?tags=' + video_tag + '&page_url=' + videoUrl + '&page_title=' + videoTitle;;
+					document.body.appendChild(tracking);
+				break;
+			}
 	  }
 	</script>
 	<script src="https://www.youtube.com/iframe_api"></script>
@@ -57,14 +53,3 @@ EOT;
 }
 add_shortcode( 'mautic_youtube', 'mautic_youtube' );
 
-
-// Autoupdater
-require 'plugin-update-checker/plugin-update-checker.php';
-$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
-	'https://github.com/powerticmkt/mautic-youtube-logger/',
-	__FILE__,
-	'mautic-youtube-logger'
-);
-
-//Optional: Set the branch that contains the stable release.
-$myUpdateChecker->setBranch('master');
